cef9d3e599830db91ac0da6fbeccdd0e6270c72b
IgorLugovoi <qeadzc4065@knu.ua>
2025-02-24 15:03:01 +0200

    Fixed some bugs in config server, added dependencies to pom xml files for config server

--- modules/movie-service/src/main/java/com/example/movie_service/controllers/MovieController.java
+++ modules/movie-service/src/main/java/com/example/movie_service/controllers/MovieController.java
@@ -15,35 +15,40 @@ import java.util.Optional;
 @RequestMapping("/api/movies")
 public class MovieController {
     private final MovieService movieService;
+
     @Autowired
-    public MovieController(MovieService movieService){
+    public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies(){
+    public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Movie>> findById(@PathVariable Long id){
-        return new ResponseEntity<>(movieService.findMovieById(id),HttpStatus.OK);
+    public ResponseEntity<Optional<Movie>> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(movieService.findMovieById(id), HttpStatus.OK);
     }
+
     @PostMapping
-    public ResponseEntity<?> createMovie(@Valid @RequestBody Movie movie){
-        return new ResponseEntity<>(movieService.createMovie(movie),HttpStatus.CREATED);
+    public ResponseEntity<?> createMovie(@Valid @RequestBody Movie movie) {
+        return new ResponseEntity<>(movieService.createMovie(movie), HttpStatus.CREATED);
     }
+
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateMovie(@Valid @RequestBody Movie movie, @PathVariable Long id){
+    public ResponseEntity<?> updateMovie(@Valid @RequestBody Movie movie, @PathVariable Long id) {
         return new ResponseEntity<>(movieService.updateMovie(movie, id), HttpStatus.CREATED);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         movieService.deleteMovieById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
     @GetMapping("/movies-by-genre/{genre}")
-    public ResponseEntity<List<Movie>> findMoviesByGenre(@PathVariable String genre){
-        return new ResponseEntity<>(movieService.findMoviesByGenre(genre),HttpStatus.OK);
+    public ResponseEntity<List<Movie>> findMoviesByGenre(@PathVariable String genre) {
+        return new ResponseEntity<>(movieService.findMoviesByGenre(genre), HttpStatus.OK);
     }
 }
